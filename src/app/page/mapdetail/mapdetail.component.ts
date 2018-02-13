4fbab1918b7500bdf8b42588a3fa13c0bbf8f0bf
Gilang Daniel <gilang@tirto.id>
2018-02-13 16:15:47 +0700

    adding route params id

--- src/app/page/mapdetail/mapdetail.component.ts
+++ src/app/page/mapdetail/mapdetail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-mapdetail',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapdetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(){
+
+  	this.sub = this.route.params.subscribe(params => {
+       this.id = +params['id']; // (+) converts string 'id' to a number
+
+       // In a real app: dispatch action to load the details here.
+    });
+    console.log(this.id);
   }
 
 }
